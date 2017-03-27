11cbce5fe042488c2c9f658228d195979f59b754
MichaelFlisar <mflisar.development@gmail.com>
2017-03-27 14:05:37 +0200

    renamed one shot to show once in the java docs

--- library/src/main/java/me/toptas/fancyshowcase/DismissListener.java
+++ library/src/main/java/me/toptas/fancyshowcase/DismissListener.java
@@ -7,13 +7,13 @@ public interface DismissListener {
     /**
      * is called when a {@link FancyShowCaseView} is dismissed
      *
-     * @param id the one shot id of the dismissed view
+     * @param id the show once id of the dismissed view
      */
     void onDismiss(String id);
     /**
      * is called when a {@link FancyShowCaseView} is skipped because of it's one shot id
      *
-     * @param id the one shot id of the dismissed view
+     * @param id the show once id of the dismissed view
      */
     void onSkipped(String id);
 }
